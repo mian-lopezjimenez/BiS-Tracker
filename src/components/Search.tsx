4c9c3c106cd92fc040b296c96d052c1d9fa397e4
Miguel Ángel López Jiménez <mian.lopezjimenez@gmail.com>
2024-10-01 19:22:59 +0200

    chore: added cache for characters

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import { SubmitHandler, useForm } from "react-hook-form";
 
 import { useAuth } from "@/hooks/index";
+import { useCharacters } from "@/stores/index";
+import { setLocalStorage } from "@/utils/localStorage";
 
 interface Inputs {
   name: string;
@@ -17,61 +19,74 @@ const Search = () => {
     formState: { errors },
   } = useForm<Inputs>();
   const { token } = useAuth();
+  const { characters, setCharacters } = useCharacters();
 
   const onSubmit: SubmitHandler<Inputs> = async (values) => {
     const { region, name, realmSlug } = values;
 
+    if (!region || !name || !realmSlug) {
+      return;
+    }
+
+    const foundCharacter = characters.find(
+      ({ character }) =>
+        character.name === name.charAt(0).toUpperCase() + name.slice(1)
+    );
+
+    if (foundCharacter) {
+      return;
+    }
+
     try {
-      const url: string = `https://${region}.api.blizzard.com/profile/wow/character/${decodeURI(
-        realmSlug.replace(/ /g, "-").toLowerCase()
-      )}/${name.toLowerCase()}/appearance?namespace=profile-${region}&locale=es_ES`;
+      const url: string = `https://${region}.api.blizzard.com/profile/wow/character/${realmSlug
+        .replace(/ /g, "-")
+        .toLowerCase()}/${name.toLowerCase()}/equipment?namespace=profile-${region}&locale=es_ES`;
       const response = await axios.get(url, {
         headers: {
-          Authorization: `Bearer ${token}`, // Añadir el token en la cabecera
+          Authorization: `Bearer ${token}`,
         },
         params: {
           namespace: "static-us",
-          locale: "es_ES", // Cambiar a 'es_MX' si deseas español
+          locale: "es_ES",
         },
       });
 
-      console.log(response.data);
+      setCharacters([...characters, response.data]);
+      setLocalStorage("characters", [...characters, response.data]);
     } catch (error) {
       console.error(`Error al obtener apariencia: ${error}`);
     }
   };
 
   return (
-    <div>
-      <form
-        className="flex justify-between rounded-full p-4 border-solid border-2 border-blue-300"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <select {...register("region", { required: true })}>
-          <option value="eu">EU</option>
-          <option value="us">US</option>
-        </select>
+    <form
+      className="flex flex-wrap justify-between rounded-lg p-4 border-solid border-2 border-blue-300"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <select {...register("region", { required: true })}>
+        <option value="eu">EU</option>
+        <option value="us">US</option>
+      </select>
 
-        <input
-          type="text"
-          className="p-2"
-          placeholder="Character Name"
-          {...register("name", { required: true })}
-        />
+      <input
+        type="text"
+        className="p-2"
+        placeholder="Character Name"
+        {...register("name", { required: true })}
+      />
 
-        <input
-          type="text"
-          className="p-2"
-          placeholder="Realm"
-          {...register("realmSlug", { required: true })}
-        />
+      <input
+        type="text"
+        className="p-2"
+        placeholder="Realm"
+        {...register("realmSlug", { required: true })}
+      />
 
-        <input
-          className="p-2 bg-teal-500 rounded-full w-20 text-white hover:bg-teal-600 transition-colors duration-300 cursor-pointer"
-          type="submit"
-        />
-      </form>
-    </div>
+      <input
+        className="p-2 bg-teal-500 rounded-full w-20 text-white hover:bg-teal-600 transition-colors duration-300 cursor-pointer"
+        type="submit"
+      />
+    </form>
   );
 };
 
